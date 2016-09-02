9bb9474d455a6f4feb63994559bea86116e7fa09
Bruno Imbrizi <brunoimbrizi@Brunos-iMac.local>
2016-09-02 16:42:24 +0100

    head.min.js needs to be loaded before reveal.js

--- src/scripts/vendor.js
+++ src/scripts/vendor.js
@@ -1,4 +1,5 @@
 global.Sketch = require('sketch-js');
 global.TweenMax = require('gsap');
 global.ControlKit = require('controlkit');
+require('./../vendor/head.min.js');
 global.Reveal = require('reveal.js');
